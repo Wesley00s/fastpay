9000b2ca5147aef2a89a76d87193f76495677843
Wesley00s <wesley300rodrigues@gmail.com>
2026-02-20 11:50:54 -0300

    init: Commit init

---
+++ src/test/java/com/fastpay/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
